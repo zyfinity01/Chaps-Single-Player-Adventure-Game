582b089e36c7f6c29cd8955c9c65303017c95c5a
Jontaieri Morris <morrisjont@ecs.vuw.ac.nz>
2022-09-06 22:32:56 +1200

    Created the empty Maven project

---
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
@@ -0,0 +1,13 @@
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
