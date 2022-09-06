23e8386cde3609c8ed3d7fd0fa1fe37426bfe0a8
Jontaieri Morris <morrisjont@ecs.vuw.ac.nz>
2022-09-06 23:00:43 +1200

    Indent using spaces

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
@@ -1,13 +1,10 @@
 package nz.ac.vuw.ecs.swen225.gp22.app;
 
 /**
- * Hello world!
- *
+ * Program entry point.
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Main  {
+  public static void main(String[] args) {
+    System.out.println( "Hello World!" );
+  }
 }
--- src/test/java/nz/ac/vuw/ecs/swen225/gp22/AppTest.java
+++ src/test/java/nz/ac/vuw/ecs/swen225/gp22/AppTest.java
@@ -5,16 +5,16 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
- */
+* Unit test for simple App.
+*/
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+  /**
+  * Rigorous Test :-)
+  */
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    assertTrue( true );
+  }
 }
