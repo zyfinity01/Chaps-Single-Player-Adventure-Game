95e43f6e35634e79e7101a3c4b1dc02f1b658195
Jontaieri Morris <morrisjont@ecs.vuw.ac.nz>
2022-09-10 13:49:01 +1200

    Added Maze constructors

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/Maze.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/Maze.java
@@ -1,10 +1,72 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Contains the state for a maze level.
  * 
  * @author Jonty Morris, 300563915.
  */
 public class Maze {
-  
+  private int rows;
+  private int cols;
+  private Tile[][] tiles;
+  private List<Tile> inventory;
+
+  /**
+   * Construct a new Maze without an inventory.
+   * @param tiles maze tiles.
+   * @param rows count of maze rows.
+   * @param cols count of maze coluns.
+   */
+  public Maze(Tile[][] tiles, int rows, int cols) {
+    this(tiles, rows, cols, new ArrayList<>());
+  }
+
+  /**
+   * Construct a new Maze with an inventory.
+   * @param tiles maze tiles.
+   * @param rows count of maze rows.
+   * @param cols count of maze coluns
+   * @param inventory player inventory.
+   */
+  public Maze(Tile[][] tiles, int rows, int cols, List<Tile> inventory) {
+    this.tiles = tiles;
+    this.rows = rows;
+    this.cols = cols;
+    this.inventory = inventory;
+  }
+
+  /**
+   * Get the number of maze rows.
+   * @return row count.
+   */
+  public int getRows() {
+    return rows;
+  }
+
+  /**
+   * Get the number of maze columns.
+   * @return column count.
+   */
+  public int getCols() {
+    return cols;
+  }
+
+  /**
+   * Get the maze tiles.
+   * @return maze tiles.
+   */
+  public Tile[][] getTiles() {
+    return tiles;
+  }
+
+  /**
+   * Get the maze inventory.
+   * @return maze inventory.
+   */
+  public List<Tile> getInventory() {
+    return inventory;
+  }
 }
