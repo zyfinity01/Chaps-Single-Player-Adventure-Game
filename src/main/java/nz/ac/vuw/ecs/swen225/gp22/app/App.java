0742529a6c64854f11a53a0ce570167ce9827083
schwablawr <schwablawr@myvuw.ac.nz>
2022-09-11 15:39:36 +1200

    fixed checkStyle to pass pipeline

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/App.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/App.java
@@ -1,31 +1,24 @@
 package nz.ac.vuw.ecs.swen225.gp22.app;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+//import java.awt.event.ItemEvent;
+//import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.*;
+//import java.awt.event.WindowAdapter;
+//import java.awt.event.WindowEvent;
+//import java.util.*;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+//import javax.swing.JPanel;
 
 // check the above imports - simply pulled them from the ones I used in A1. Not all will be used but put them there for simplicity. 
 
-public class App implements KeyListener {
+public class App extends JFrame implements KeyListener {
 
   // double check visibility :-)
   private int timeLeftToPlay;
   private int numKeysCollected;
   private int numTreasureLeft;
-
-  public App() {
-
-  }
 
   @Override
   public void keyTyped(KeyEvent e) {
@@ -53,5 +46,26 @@ public class App implements KeyListener {
   public int getTimeLeft() {
     return timeLeftToPlay;
   }
+
+  /**
+   * Gets the number of keys collected.
+   * 
+   * @return numKeysCollected
+   */
+  public int getNumKeysCollected() {
+    return numKeysCollected;
+  }
+  
+  /**
+   * Gets the number of chips(treasure) left.
+   * 
+   * @return numTreasureLeft
+   */
+  public int getNumTreasureLeft() {
+    return numTreasureLeft;
+  }
+
+
+
 
 }
