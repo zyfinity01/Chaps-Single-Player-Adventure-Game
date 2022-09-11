237f2999d5718f8f086a46326ff6be34daef18f8
schwablawr <schwablawr@myvuw.ac.nz>
2022-09-11 15:29:50 +1200

    created App file, with basic methods

--- src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/domain/fuzz/Fuzz.java
+++ src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/domain/fuzz/Fuzz.java
@@ -1,11 +1,21 @@
 package test.nz.ac.vuw.ecs.swen225.gp22.domain.fuzz;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import org.junit.Test;
+
 /**
  * @author Lawrence Schwabe, 300570719.
  * 
  * 
  */
 public class Fuzz {
+
+    
+
+
 
   
 }
