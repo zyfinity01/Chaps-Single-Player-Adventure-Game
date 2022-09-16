d0d4ef9d60535d2debaf05cfc831d31d299e6c47
North <northgeor@ecs.vuw.ac.nz>
2022-09-16 13:51:54 +1200

    Define interface

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -1,13 +1,24 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 
+import java.awt.*;
+
+interface Renderer { void render (Maze Maze, Graphics graphics); }
 
 /**
  * @author Lawrence Schwabe (at date of file creation), 300570719.
  * 
  */
-public class Renderer {
+class RendererImpl implements Renderer{
   
+    public void render(Maze maze, Graphics graphics) {
 
+        for (int x=0; x<maze.getCols(); x++) {
+            for (int y=0; y<maze.getRows(); y++) {
+
+            }
+        }
+    }
 
 
 }
