84870207969f3ad7bba16cd5cd0a6415d7b776ff
North <northgeor@ecs.vuw.ac.nz>
2022-09-16 15:59:37 +1200

    Write the skeleton. TODO:  - Find tileset  - cleanup/remove TileImage enum  - rescale the resulting image  - actually make it work

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -1,24 +1,46 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
 
-interface Renderer { void render (Maze Maze, Graphics graphics); }
+enum TileImage {
+    // TODO: Find tileset
+    // Load images in advance, to save time
+    // TODO: this is kinda messy
+    Wall {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Empty {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Key {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Door {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Info {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Treasure {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Lock {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Exit {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }},
+    Chap {{ try { img = ImageIO.read(new File("images/wall")); } catch (IOException ignored) { } }};
 
-/**
- * @author Lawrence Schwabe (at date of file creation), 300570719.
- * 
- */
-class RendererImpl implements Renderer{
-  
-    public void render(Maze maze, Graphics graphics) {
+    BufferedImage img;
+    public BufferedImage image() { return img; }
+}
+
+public class Renderer {
+    int windowWidth = 20;
+    int tileWidth = 20;
+
+    void render (Maze maze, Canvas canvas){
+        Graphics2D image = new BufferedImage(windowWidth*tileWidth, windowWidth*tileWidth, BufferedImage.TYPE_4BYTE_ABGR).createGraphics();
 
         for (int x=0; x<maze.getCols(); x++) {
             for (int y=0; y<maze.getRows(); y++) {
-
+                image.drawImage(TileImage.valueOf(maze.getTiles()[x][y].type()), null, x*tileWidth, y*tileWidth);
             }
         }
+
+        // TODO: I'll probably need to rescale this
+        canvas.paint(image);
     }
-
-
 }
+
