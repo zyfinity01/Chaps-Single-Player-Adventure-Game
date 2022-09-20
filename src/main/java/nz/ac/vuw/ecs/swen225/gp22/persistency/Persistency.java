cde49e2a0cf53b57239929423ae955fb4ff201e4
Niraj <ngandhi360@gmail.com>
2022-09-21 00:45:12 +1200

    code cleanup, added game save test

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
@@ -17,13 +17,14 @@ public class Persistency {
 
     /**
      * This method is used to save the game status in the form of XML file.
-     * @param fileName name of output file
+     *
+     * @param fileName  name of output file
      * @param levelName name of level - String or Int to be decided
-     * @param time Either time taken to complete level or time left as spare
-     * @param moves number of moves taken to complete level
-     * @param chips number of computer chips collected
-     * @param lives number of lives left
-     * @param board 2D array of tiles
+     * @param time      Either time taken to complete level or time left as spare
+     * @param moves     number of moves taken to complete level
+     * @param chips     number of computer chips collected
+     * @param lives     number of lives left
+     * @param board     2D array of tiles
      */
     public void saveGameStatus(String fileName, String levelName, int time, int moves, int chips, int lives, Tile[][] board) {
         //create root element
@@ -57,52 +58,28 @@ public class Persistency {
                 Element tile = new Element("tile");
                 tile.setText(operatingTile.getClass().toString());
                 Field[] fields = operatingTile.getClass().getDeclaredFields();
-                //add all fields to tile element and all values of the fields
                 for (Field field : fields) {
                     field.setAccessible(true);
-                    Element fieldElement = new Element(field.getName());
                     try {
-                        //if field is of type list, then traverse through the list and add all elements to field element
-                        if (field.getType().toString().contains("List")) {
-                            List list = (List) field.get(operatingTile);
-                            for (Object object : list) {
-                                Element listElement = new Element("listElement");
-                                listElement.setText(object.toString());
-                                fieldElement.addContent(listElement);
-                            }
-                        }
-                        //if field is of type int, then add value to field element
-                        else if (field.getType().toString().contains("int")) {
-                            fieldElement.setText(Integer.toString(field.getInt(operatingTile)));
-                        }
-                        //if field is of type boolean, then add value to field element
-                        else if (field.getType().toString().contains("boolean")) {
-                            fieldElement.setText(Boolean.toString(field.getBoolean(operatingTile)));
-                        }
-                        //if field is of type String, then add value to field element
-                        else if (field.getType().toString().contains("String")) {
-                            fieldElement.setText((String) field.get(operatingTile));
-                        }
-                        //if field is of type Map, traverse through the Map and add all key and value to field element
-                        else if (field.getType().toString().contains("Map")) {
-                        }
-                        fieldElement.addContent(field.get(operatingTile).toString());
-
+                        Element fieldElement = new Element(field.getName());
+                        fieldElement.setText(field.get(operatingTile).toString());
+                        tile.addContent(fieldElement);
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                     }
-                    tile.addContent(fieldElement);
                 }
-                boardElement.addContent(tile);
+                Element boardLocation = new Element("boardLocation");
+                boardLocation.addContent(i + "," + j);
+                tile.addContent(boardLocation);
+                //save document to XML file
+                try {
+                    XMLOutputter xmlOutput = new XMLOutputter();
+                    xmlOutput.setFormat(Format.getPrettyFormat());
+                    xmlOutput.output(doc, new FileWriter(fileName));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        //save document to XML file
-        try {
-            XMLOutputter xmlOutput = new XMLOutputter();
-            xmlOutput.setFormat(Format.getPrettyFormat());
-            xmlOutput.output(doc, new FileWriter(fileName));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
