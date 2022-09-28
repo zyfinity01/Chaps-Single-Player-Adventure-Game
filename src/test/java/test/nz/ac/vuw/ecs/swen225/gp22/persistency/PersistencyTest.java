c8f28b8da6c32554854b8b6ed72c2cc8cd5c22dc
Niraj Gandhi <gandhinira@ecs.vuw.ac.nz>
2022-09-28 21:08:47 +1300

    test generates maze object

--- src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
+++ src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
@@ -10,22 +10,31 @@ import org.junit.jupiter.api.Test;
  */
 
 public class PersistencyTest {
-    @Test
-    public void test1() {
-        Tile[][] board = new Tile[10][10];
-        //iterate over board and create tile objects
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                board[i][j] = new Wall();
-            }
-        }
-        new Persistency().saveGameStatus("test1.xml", "level1", 100, 10, 5, 3, board);
-
-    }
+//    @Test
+//    public void test1() {
+//        Tile[][] board = new Tile[10][10];
+//        //iterate over board and create tile objects
+//        for(int i = 0; i < 10; i++) {
+//            for(int j = 0; j < 10; j++) {
+//                board[i][j] = new Wall();
+//            }
+//        }
+//        new Persistency().saveGameStatus("test1.xml", "level1", 100, 10, 5, 3, board);
+//    }
 
     @Test
     public void test2() {
-        // TODO
+        Maze myMaze = new Persistency().loadGame("level1.xml", 10, 10);
+        Tile[][] tiles = myMaze.getTiles();
+        //iterate over tiles length and width and print them
+        for(int i = 0; i < tiles.length; i++) {
+            for(int j = 0; j < tiles[i].length; j++) {
+                //System.out.print(tiles[i][j].getClass().getSimpleName() + " ");
+                if(tiles[i][j] == null) System.out.println("x:" + i + " y:" + j +" : null tile");
+                else System.out.println("x:" + i + " y:" + j + " : " + tiles[i][j].toString());
+            }
+        }
+
 
     }
 }
