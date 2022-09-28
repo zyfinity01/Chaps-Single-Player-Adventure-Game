6ab94111c61cfa8dd80a1b7b68117ffd7cad3fea
Niraj Gandhi <gandhinira@ecs.vuw.ac.nz>
2022-09-28 21:18:04 +1300

    Update src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
@@ -20,76 +20,6 @@ import java.util.List;
  * @author  Niraj Gandhi, 300564849.
  */
 public class Persistency {
-
-
-    /**
-     * This method is used to save the game status in the form of XML file.
-     *
-     * @param fileName  name of output file
-     * @param levelName name of level - String or Int to be decided
-     * @param time      Either time taken to complete level or time left as spare
-     * @param moves     number of moves taken to complete level
-     * @param chips     number of computer chips collected
-     * @param lives     number of lives left
-     * @param board     2D array of tiles
-     */
-    public void saveGameStatus(String fileName, String levelName, int time, int moves, int chips, int lives, Tile[][] board) {
-        //create root element
-        Element root = new Element("game");
-        //create document
-        Document doc = new Document(root);
-        //create child elements
-        Element level = new Element("level");
-        Element timeElement = new Element("time");
-        Element movesElement = new Element("moves");
-        Element chipsElement = new Element("chips");
-        Element livesElement = new Element("lives");
-        Element boardElement = new Element("board");
-        //add child elements to root element
-        root.addContent(level);
-        root.addContent(timeElement);
-        root.addContent(movesElement);
-        root.addContent(chipsElement);
-        root.addContent(livesElement);
-        root.addContent(boardElement);
-        //add values to child elements
-        level.setText(levelName);
-        timeElement.setText(Integer.toString(time));
-        movesElement.setText(Integer.toString(moves));
-        chipsElement.setText(Integer.toString(chips));
-        livesElement.setText(Integer.toString(lives));
-        //add board to board element
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                Tile operatingTile = board[i][j];
-                Element tile = new Element("tile");
-                tile.setText(operatingTile.getClass().toString());
-                Field[] fields = operatingTile.getClass().getDeclaredFields();
-                for (Field field : fields) {
-                    field.setAccessible(true);
-                    try {
-                        Element fieldElement = new Element(field.getName());
-                        fieldElement.setText(field.get(operatingTile).toString());
-                        tile.addContent(fieldElement);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-                Element boardLocation = new Element("boardLocation");
-                boardLocation.addContent(i + "," + j);
-                tile.addContent(boardLocation);
-                //save document to XML file
-                try {
-                    XMLOutputter xmlOutput = new XMLOutputter();
-                    xmlOutput.setFormat(Format.getPrettyFormat());
-                    xmlOutput.output(doc, new FileWriter(new File("src/levels/", "level1.xml")));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     /**
      * This method is used to load the game status from the XML file.
      *
@@ -125,19 +55,19 @@ public class Persistency {
                                 String text;
                                 System.out.println(tile.getName());
                                 switch (tile.getName()) {
+                                    case "TREASURE" -> board[x][y] = new Treasure();
                                     case "WALL" -> board[x][y] = new Wall();
+                                    case "LOCK" -> board[x][y] = new Lock();
+                                    case "EXIT" -> board[x][y] = new Exit();
+                                    case "CHAP" -> board[x][y] = new Chap();
                                     case "KEY" -> {
                                         c = Color.valueOf(tile.getChild("color").getValue());
                                         board[x][y] = new Key(c);
                                     }
-                                    case "LOCK" -> board[x][y] = new Lock();
                                     case "INFO" -> {
                                         text = tile.getChild("text").getValue();
                                         board[x][y] = new Info(text);
                                     }
-                                    case "TREASURE" -> board[x][y] = new Treasure();
-                                    case "EXIT" -> board[x][y] = new Exit();
-                                    case "CHAP" -> board[x][y] = new Chap();
                                 }
                             }
                             break;
