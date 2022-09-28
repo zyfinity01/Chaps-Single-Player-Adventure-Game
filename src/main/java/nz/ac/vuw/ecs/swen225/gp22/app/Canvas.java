e91b9667666b2d41433441a6e0a1c76daf0a30bd
North <northgeor@ecs.vuw.ac.nz>
2022-09-28 16:48:06 +1300

    style now checked

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
@@ -1,13 +1,9 @@
 package nz.ac.vuw.ecs.swen225.gp22.app;
 
-import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Wall;
-import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
-
 import java.awt.*;
 import javax.swing.JPanel;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
+import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 
 /**
  * Canvas to draw on.
@@ -18,8 +14,7 @@ public class Canvas extends JPanel {
 
   @Override
   public void paint(Graphics graphics) {
-    // TODO: Pass details needed
-    Tile[][] tiles = {{new Wall(), new Chap()}, {null, new Wall()}};
-    Renderer.render(new Maze(tiles, 2, 2), (Graphics2D) graphics);
+    // TODO: Pass in correct maze
+    Renderer.render(null, (Graphics2D) graphics);
   }
 }
--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -1,76 +1,98 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
-import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
+
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-
+import javax.imageio.ImageIO;
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 
 // I've made this static because there's really no need to store state
 // TODO: There might be
+// TODO: Better comments
+
+/** Handles rendering. */
 public class Renderer {
 
-    // TODO: rescale/crop to fit
-    static int windowWidth = 20;
-    static int tileWidth = 40;
+  // TODO: rescale/crop to fit
+  static int windowWidth = 20;
+  static int tileWidth = 40;
 
-    // Cached tiles
-    // TODO: Find tileset
-    static BufferedImage free = null;
-    static BufferedImage wall;
-    static BufferedImage key;
-    static BufferedImage door;
-    static BufferedImage lock;
-    static BufferedImage info;
-    static BufferedImage treasure;
-    static BufferedImage exit;
-    static BufferedImage chap;
-    // load images
-    static { try {
-        String s = Paths.get("").toAbsolutePath().toString();
-//        free = ImageIO.read(new File("images//free.png"));
-        wall = ImageIO.read(new File("images//wall.png"));
-        key = ImageIO.read(new File("images//wall.png"));
-        door = ImageIO.read(new File("images//wall.png"));
-        lock = ImageIO.read(new File("images//wall.png"));
-        info = ImageIO.read(new File("images//wall.png"));
-        treasure = ImageIO.read(new File("images//wall.png"));
-        exit = ImageIO.read(new File("images//wall.png"));
-        chap = ImageIO.read(new File("images//chap.png"));
-    } catch (IOException e) { e.printStackTrace(); }}
+  // Cached tiles
+  // TODO: Find tileset
+  static BufferedImage free = null;
+  static BufferedImage wall;
+  static BufferedImage key;
+  static BufferedImage door;
+  static BufferedImage lock;
+  static BufferedImage info;
+  static BufferedImage treasure;
+  static BufferedImage exit;
+  static BufferedImage chap;
 
-    public static BufferedImage image(Tile tile) {
-        // can't switch on instanceof
-        if (tile instanceof Wall) { return wall; }
-        if (tile instanceof Key) { return key; }
-        if (tile instanceof Door) { return door; }
-        if (tile instanceof Lock) { return lock; }
-        if (tile instanceof Info) { return info; }
-        if (tile instanceof Treasure) { return treasure; }
-        if (tile instanceof Exit) { return exit; }
-        if (tile instanceof Chap) {
-            // TODO: switch on direction
-            return chap;
-        }
-        return free;        // instanceof null || fallback
+  // load images
+  static {
+    try {
+      String s = Paths.get("").toAbsolutePath().toString();
+      // free = ImageIO.read(new File("images//free.png"));
+      wall = ImageIO.read(new File("images//wall.png"));
+      key = ImageIO.read(new File("images//wall.png"));
+      door = ImageIO.read(new File("images//wall.png"));
+      lock = ImageIO.read(new File("images//wall.png"));
+      info = ImageIO.read(new File("images//wall.png"));
+      treasure = ImageIO.read(new File("images//wall.png"));
+      exit = ImageIO.read(new File("images//wall.png"));
+      chap = ImageIO.read(new File("images//chap.png"));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public static void render (Maze maze, Graphics2D image){
-        // Affine transformation is to rescale the image - https://www.geogebra.org/m/Fq8zyEgS
-        // new AffineTransformOp(new AffineTransform(0.1,0,0,0.1,0,0), AffineTransformOp.TYPE_BILINEAR)
-        image.drawImage(free, null,20, 20);
-        for (int x=0; x<maze.getCols(); x++) {
-            for (int y=0; y<maze.getRows(); y++) {
-                image.drawImage(image(maze.getTiles()[y][x]), null, x*tileWidth, y*tileWidth);
-            }
-        }
-        image.drawImage(chap, null, maze.getChapPosition().x()*tileWidth, maze.getChapPosition().y()*tileWidth);
+  }
+
+  /** get the image for a given tile. */
+  public static BufferedImage image(Tile tile) {
+    // can't switch on instanceof
+    if (tile instanceof Wall) {
+      return wall;
     }
+    if (tile instanceof Key) {
+      return key;
+    }
+    if (tile instanceof Door) {
+      return door;
+    }
+    if (tile instanceof Lock) {
+      return lock;
+    }
+    if (tile instanceof Info) {
+      return info;
+    }
+    if (tile instanceof Treasure) {
+      return treasure;
+    }
+    if (tile instanceof Exit) {
+      return exit;
+    }
+    if (tile instanceof Chap) {
+      // TODO: switch on direction
+      return chap;
+    }
+    return free;        // instanceof null || fallback
+  }
+
+  /** Render. */
+  public static void render(Maze maze, Graphics2D image) {
+    // Affine transformation is to rescale the image - https://www.geogebra.org/m/Fq8zyEgS
+    // new AffineTransformOp(new AffineTransform(0.1,0,0,0.1,0,0), AffineTransformOp.TYPE_BILINEAR)
+    image.drawImage(free, null, 20, 20);
+    for (int x = 0; x < maze.getCols(); x++) {
+      for (int y = 0; y < maze.getRows(); y++) {
+        image.drawImage(image(maze.getTiles()[y][x]), null, x * tileWidth, y * tileWidth);
+      }
+    }
+    image.drawImage(chap, null,
+        maze.getChapPosition().x() * tileWidth, maze.getChapPosition().y() * tileWidth);
+  }
 }
 
