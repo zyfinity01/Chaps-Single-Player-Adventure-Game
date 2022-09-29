831c587e5b60a4048f7cd6579574e064632f3056
Sam Redmond <redmonsam@myvuw.ac.nz>
2022-09-29 14:00:03 +1300

    fix spotbugs

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
@@ -3,6 +3,9 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
 import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 
 /**
@@ -14,6 +17,11 @@ public class Canvas extends JPanel {
 
   @Override
   public void paint(Graphics graphics) {
-    Renderer.render(null, (Graphics2D) graphics);
+    /* Temporary to avoid spotbugs while Maze link is being made */
+    var tiles = new Tile[1][1];
+    tiles[0][0] = new Chap();
+    var maze = new Maze(tiles, 1, 1);
+
+    Renderer.render(maze, (Graphics2D) graphics);
   }
 }
--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -59,7 +59,7 @@ public class Renderer {
   }
 
   /** get the image for a given tile. */
-  public static BufferedImage image(Tile tile) {
+  private static BufferedImage image(Tile tile) {
     // can't switch on instanceof
     if (tile instanceof Wall) {
       return wall;
