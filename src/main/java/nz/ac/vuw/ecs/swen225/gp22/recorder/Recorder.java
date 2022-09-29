9946eb50f5251666ce858897528812cd309340ab
westshae <shaewest02@gmail.com>
2022-09-29 13:45:58 +1300

    Checkstyles + basic movement saving to xml

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -1,4 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
+
+import java.io.FileWriter;
 import java.util.ArrayList;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -6,41 +8,71 @@ import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 /**
- * @author Shae West, 300565911
+   * Recorder object saves, stores and exports all movements made by users and players.
+   * $ @author Shae West, 300565911
  */
 public class Recorder {
-  ArrayList savedSnapshots;
+  ArrayList<String> savedSnapshots;
   String recordingName;
   /**
    * Creates an Recorder,
    * If recordingName matches any current .XML files, load Recording from file.
    * Else, create a new empty Recorder object to be saved at a later date.
-   * @param recordingName Name of XML file.
+   * $ @param recordingName Name of XML file.
    */
-  public Recorder(String recordingName){
-    recordingName.toLowerCase();
+  
+  public Recorder(String recordingName) {
+    //recordingName.toLowerCase();
+    //TODO: Implement loading from file via string name
+    if (recordingName.equals("ASavedFile"))  {
+      this.loadFromXml(recordingName);
+    } else  {
+      savedSnapshots = new ArrayList<>();
+    }
   }
 
   /**
-   * Saves (all actions, or state each tick) 
+   * Saves (all actions, or state each tick).
    * TODO: Decide what is passed as argument to save
    */
-  public void saveSnapshot(){
-
+  public void saveMovement(String movementType) {
+    //TODO: Probably changing to ENUM saving.
+    savedSnapshots.add(movementType);
   }
 
   /**
-   * Saves the current savedSnapshots to XML format
+   * Saves the current savedSnapshots to XML format.
    */
-  public void saveToXML(){
+  public void saveToXml() {
+    //create document
+    Document document = new Document();
+    Element root = new Element("root");
+    Element moves = new Element("moves");
+    for (String current : savedSnapshots) {
+      moves.addContent(new Element("move").setText(current));
+    }
+    root.addContent(moves);
+
+    document.setContent(root);
+
+    try {
+      FileWriter writer = new FileWriter("userinfo.xml");
+      XMLOutputter outputter = new XMLOutputter();
+      outputter.setFormat(Format.getPrettyFormat());
+      outputter.output(document, writer);
+      outputter.output(document, System.out);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
 
   }
 
   /**
    * Loads local .XML recording if it exists
-   * @param recordingName Used to find local .XML saved recording
+   * $ @param recordingName Used to find local .XML saved recording
    */
-  private void loadFromXML(String recordingName){
-
+  private void loadFromXml(String recordingName){
+    //TODO
   }
 }
