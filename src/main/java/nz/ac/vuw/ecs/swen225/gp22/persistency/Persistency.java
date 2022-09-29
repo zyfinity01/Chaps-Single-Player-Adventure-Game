9798ad5208e81d7d064a40fad73517380930a6dd
Sam Redmond <redmonsam@myvuw.ac.nz>
2022-09-30 11:19:17 +1300

    fix checkstyle

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
@@ -1,19 +1,31 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
-import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Color;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Exit;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Info;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Key;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Lock;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Treasure;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Wall;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.DOMBuilder;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.util.List;
 
 /**
-* @author  Niraj Gandhi, 300564849.
-*/
+ * Persistency module.
+ *
+ * @author  Niraj Gandhi, 300564849.
+ */
 public class Persistency {
   /**
   * This method is used to load the game status from the XML file.
@@ -23,51 +35,56 @@ public class Persistency {
   public Maze loadGame(String fileName, int boardCols, int boardRows) {
     Tile[][] board = new Tile[boardRows][boardCols];
     //read XML file
-    Document doc = getParsedDoc("src/levels/"+fileName);
-    for(Element element : doc.getRootElement().getChildren()){
+    Document doc = getParsedDoc("src/levels/" + fileName);
+    for (Element element : doc.getRootElement().getChildren()) {
       System.out.println(element.getName() + " " + element.getText());
       switch (element.getName()) {
         //TODO
         case "level":
-        break;
+          break;
         case "time":
-        break;
+          break;
         case "moves":
-        break;
+          break;
         case "chips":
-        break;
+          break;
         case "lives":
-        break;
+          break;
         case "board":
-        List<Element> boardElements = element.getChildren();
-        for (Element item : boardElements) {
-          if (item.getName().equals("tiles")) {
-            List<Element> tileElements = item.getChildren();
-            for (Element tile : tileElements) {
-              int x = Integer.parseInt(tile.getChild("x").getValue());
-              int y = Integer.parseInt(tile.getChild("y").getValue());
-              Color c;
-              String text;
-              System.out.println(tile.getName());
-              switch (tile.getName()) {
-                case "TREASURE" -> board[x][y] = new Treasure();
-                case "WALL" -> board[x][y] = new Wall();
-                case "LOCK" -> board[x][y] = new Lock();
-                case "EXIT" -> board[x][y] = new Exit();
-                case "CHAP" -> board[x][y] = new Chap();
-                case "KEY" -> {
-                  c = Color.valueOf(tile.getChild("color").getValue());
-                  board[x][y] = new Key(c);
-                }
-                case "INFO" -> {
-                  text = tile.getChild("text").getValue();
-                  board[x][y] = new Info(text);
+          List<Element> boardElements = element.getChildren();
+          for (Element item : boardElements) {
+            if (item.getName().equals("tiles")) {
+              List<Element> tileElements = item.getChildren();
+              for (Element tile : tileElements) {
+                int x = Integer.parseInt(tile.getChild("x").getValue());
+                int y = Integer.parseInt(tile.getChild("y").getValue());
+                Color c;
+                String text;
+                System.out.println(tile.getName());
+                switch (tile.getName()) {
+                  case "TREASURE" -> board[x][y] = new Treasure();
+                  case "WALL" -> board[x][y] = new Wall();
+                  case "LOCK" -> board[x][y] = new Lock();
+                  case "EXIT" -> board[x][y] = new Exit();
+                  case "CHAP" -> board[x][y] = new Chap();
+                  case "KEY" -> {
+                    c = Color.valueOf(tile.getChild("color").getValue());
+                    board[x][y] = new Key(c);
+                  }
+                  case "INFO" -> {
+                    text = tile.getChild("text").getValue();
+                    board[x][y] = new Info(text);
+                  }
+                  default -> {
+                  }
                 }
               }
+              break;
             }
-            break;
           }
-        }
+          break;
+        default:
+          break;
       }
     }
     return new Maze(board, boardRows, boardCols);
@@ -80,8 +97,7 @@ public class Persistency {
       DocumentBuilder documentBuilder = factory.newDocumentBuilder();
       org.w3c.dom.Document w3cDocument = documentBuilder.parse(fileName);
       doc = new DOMBuilder().build(w3cDocument);
-    }
-    catch (IOException | SAXException | ParserConfigurationException e) {
+    } catch (IOException | SAXException | ParserConfigurationException e) {
       e.printStackTrace();
     }
     return doc;
