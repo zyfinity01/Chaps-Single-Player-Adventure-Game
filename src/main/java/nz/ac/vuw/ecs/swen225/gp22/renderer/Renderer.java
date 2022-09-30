0557f10aaa5be1ddaa3174767135dae923350de1
Cynosure-North <52474045+Cynosure-North@users.noreply.github.com>
2022-09-30 16:08:38 +1300

    fix bug and add yellow

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -42,9 +42,11 @@ public class Renderer {
   static BufferedImage key_red;
   static BufferedImage key_blue;
   static BufferedImage key_green;
+  static BufferedImage key_yellow;
   static BufferedImage door_red;
   static BufferedImage door_blue;
   static BufferedImage door_green;
+  static BufferedImage door_yellow;
 
   // load images
   static {
@@ -61,9 +63,11 @@ public class Renderer {
       key_red = ImageIO.read(new File("images//key_red.png"));
       key_blue = ImageIO.read(new File("images//key_blue.png"));
       key_green = ImageIO.read(new File("images//key_green.png"));
+      key_yellow = ImageIO.read(new File("images//key_yellow.png"));
       door_red = ImageIO.read(new File("images//door_red.png"));
       door_blue = ImageIO.read(new File("images//door_blue.png"));
       door_green = ImageIO.read(new File("images//door_green.png"));
+      door_yellow = ImageIO.read(new File("images//door_yellow.png"));
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -77,6 +81,8 @@ public class Renderer {
     }
     if (tile instanceof Key key) {
       switch (key.color()) {
+        case Yellow:
+          return key_yellow;
         case Blue:
           return key_blue;
         case Green:
@@ -88,13 +94,15 @@ public class Renderer {
     }
     if (tile instanceof Door door) {
       switch (door.color()) {
+        case Yellow:
+          return door_yellow;
         case Blue:
-          return key_blue;
+          return door_blue;
         case Green:
-          return key_green;
+          return door_green;
         case Red:
         default:
-          return key_red;
+          return door_red;
       }
     }
     if (tile instanceof Lock) {
@@ -123,7 +131,7 @@ public class Renderer {
     image.drawImage(free, null, 20, 20);
     for (int x = 0; x < maze.getCols(); x++) {
       for (int y = 0; y < maze.getRows(); y++) {
-        image.drawImage(image(maze.getTiles()[y][x]), null, x * tileWidth, y * tileWidth);
+        image.drawImage(image(maze.getTiles()[x][y]), null, x * tileWidth, y * tileWidth);
       }
     }
     image.drawImage(chap, null, maze.getChapPosition().x() * tileWidth,
