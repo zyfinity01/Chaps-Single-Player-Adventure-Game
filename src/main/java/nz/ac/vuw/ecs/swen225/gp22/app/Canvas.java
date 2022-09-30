2868e1b43d045b59449fcfc3ec17fe70c0306cbd
Cynosure-North <52474045+Cynosure-North@users.noreply.github.com>
2022-09-30 14:22:25 +1300

    update icons

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Canvas.java
@@ -2,10 +2,10 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Collection;
 import javax.swing.JPanel;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 
 /**
@@ -18,9 +18,19 @@ public class Canvas extends JPanel {
   @Override
   public void paint(Graphics graphics) {
     /* Temporary to avoid spotbugs while Maze link is being made */
-    var tiles = new Tile[1][1];
-    tiles[0][0] = new Chap();
-    var maze = new Maze(tiles, 1, 1);
+//    var tiles = new Tile[10][10];
+    Tile[][] tiles = new Tile[][]{  {new Wall(), new Wall(),new Wall(), new Wall(), new Wall(), new Wall(),           new Wall(),            new Wall(),     new Wall(),         new Wall(),}
+                                  , {new Wall(), null,      null,       null,       new Wall(), new Chap(),           new Info("test"), new Treasure(), new Key(Color.Blue), new Wall()}
+                                  , {new Wall(), null,      new Exit(), null,       new Wall(), null,                 null,                  null,           null,               new Wall()}
+                                  , {new Wall(), null,      null      , null,       new Wall(), new Door(Color.Blue), new Wall(),            new Lock(),     new Wall(),         new Wall()}
+//                                  , {new Wall(),new Wall()}
+//                                  , {new Wall(),new Wall()}
+//                                  , {new Wall(),new Wall()}
+//                                  , {new Wall(),new Wall()}
+//                                  , {new Wall(),new Wall()}
+//                                  , {new Wall(),new Wall()}
+    };
+    var maze = new Maze(tiles, 4, 10);
 
     Renderer.render(maze, (Graphics2D) graphics);
   }
