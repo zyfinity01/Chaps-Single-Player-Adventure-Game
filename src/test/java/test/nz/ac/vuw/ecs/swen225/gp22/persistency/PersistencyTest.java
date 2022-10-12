cd965163c36d17ac2b7ed7ff2fccdb1937876a61
Niraj <ngandhi360@gmail.com>
2022-10-13 03:21:29 +1300

    added junit test

--- src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
+++ src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
@@ -2,9 +2,18 @@ package test.nz.ac.vuw.ecs.swen225.gp22.persistency;
 
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 import nz.ac.vuw.ecs.swen225.gp22.persistency.Persistency;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
@@ -43,4 +52,57 @@ public class PersistencyTest {
 //
 //
 //    }
+    @Test
+    public void testWallLoading_01() {
+        int x = 5;
+        int y = 5;
+        int chapX = 1;
+        int chapY = 1;
+        int col = 17;
+        int row = 17;
+        // Creating json wall tile object
+        toXml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<game>\n" +
+                "    <board>\n" +
+                "        <tiles>\n" +
+                "            <WALL>\n" +
+                "                <x>" + x + "</x>\n" +
+                "                <y>" + y + "</y>\n" +
+                "            </WALL>\n" +
+                "            <CHAP>\n" +
+                "                <x>" + chapX + "</x>\n" +
+                "                <y>" + chapY + "</y>\n" +
+                "            </CHAP>\n" +
+                "        </tiles>\n" +
+                "    </board>\n" +
+                "</game>\n");
+        String fileName = "persistencyTest.xml";
+        Maze maze = Persistency.loadGame(fileName, col, row);
+        System.out.println(Arrays.deepToString(maze.getTiles()));
+        assertTrue(getTile(maze, x, y) instanceof Wall);
+    }
+
+    public static Tile getTile(Maze maze, int x, int y) {
+        Tile[][] tiles = maze.getTiles();
+        for(int i = 0; i < tiles.length; i++) {
+            for(int j = 0; j < tiles[i].length; j++) {
+                if(i == x && j == y) {
+                    return tiles[i][j];
+                }
+            }
+        }
+        return null;
+    }
+    public static void toXml(String s) {
+        try {
+            FileOutputStream fileStream = new FileOutputStream("src/levels/persistencyTest.xml");
+            OutputStreamWriter writer;
+            writer = new OutputStreamWriter(fileStream, StandardCharsets.UTF_8);
+            writer.write(s);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
