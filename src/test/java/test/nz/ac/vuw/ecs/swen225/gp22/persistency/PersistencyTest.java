81a33d7ee4228da5d51c00bbe678988bf87afafe
Niraj <ngandhi360@gmail.com>
2022-10-16 16:24:02 +1300

    cleaned up persistency module related code, made file pathing more consistent and removed redundant code.

--- src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
+++ src/test/java/test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTest.java
@@ -18,38 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 public class PersistencyTest {
-//    @Test
-//    public void test1() {
-//        Persistency.saveGame("test5.xml", Persistency.loadGame("level1.xml", 17, 17));
-//    }
-//    @Test
-//    public void test1() {
-//        Tile[][] board = new Tile[10][10];
-//        //iterate over board and create tile objects
-//        for(int i = 0; i < 10; i++) {
-//            for(int j = 0; j < 10; j++) {
-//                board[i][j] = new Wall();
-//            }
-//        }
-//        new Persistency().saveGameStatus("test1.xml", "level1", 100, 10, 5, 3, board);
-//    }
-
-//    @Test
-//    public void test2() {
-//        Maze myMaze = new Persistency().loadGame("level1.xml", 10, 10);
-//        Tile[][] tiles = myMaze.getTiles();
-//        //iterate over tiles length and width and print them
-//        for(int i = 0; i < tiles.length; i++) {
-//            for(int j = 0; j < tiles[i].length; j++) {
-//                //System.out.print(tiles[i][j].getClass().getSimpleName() + " ");
-//                if(tiles[i][j] == null) System.out.println("x:" + i + " y:" + j +" : null tile");
-//                else System.out.println("x:" + i + " y:" + j + " : " + tiles[i][j].toString());
-//            }
-//        }
-//
-//
-//    }
-    //Save toXML to file
     private static final String fileName = "src/levels/persistencyTest.xml";
     private static final String saveGameFileName = "src/levels/saveGamePersistencyTest.xml";
 
@@ -545,7 +513,7 @@ public class PersistencyTest {
     }
     public static void toXml(String s) {
         try {
-            FileOutputStream fileStream = new FileOutputStream("src/levels/persistencyTest.xml");
+            FileOutputStream fileStream = new FileOutputStream(fileName);
             OutputStreamWriter writer;
             writer = new OutputStreamWriter(fileStream, StandardCharsets.UTF_8);
             writer.write(s);
